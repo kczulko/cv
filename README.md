f7b93e62b218c37c1acd95849b6e5e200bec47b0
Karol Czulkowski <karol.czulkowski@gmail.com>
2019-12-19 13:56:04 +0100

    Add initial version of cv content.

---
+++ README.md
@@ -0,0 +1,31 @@
+# kczulko cv
+
+## Secret data encryption
+
+1. Install git-crypt - I store my secret data within `secrets.nix` which has following content:
+
+``` nix
+{
+  DATE_OF_BIRTH    = "January 1st 1900";
+  CELLPHONE        = "999 999 999";
+  ADDRESS_STREET   = ...;
+  ADDRESS_ZIP_CITY = ...;
+  ADDRESS_COUNTRY  = ...;
+}
+
+```
+
+2. Import symmetric key for this repository:
+
+``` bash
+git-crypt unlock your_symmetric.key
+```
+
+## BUILD
+
+Execute:
+
+```bash
+nix-shel --command 'lualatex -interaction=nonstopmode kczulko.tex'
+```
+
--- shell.nix
+++ shell.nix
@@ -1,27 +1,36 @@
-{ pkgs ? import <nixpkgs> {} }: with pkgs;
+{ pkgs ? import (builtins.fetchTarball {
+  name = "nixos-unstable-2019-12-05";
+  url =  https://github.com/nixos/nixpkgs/archive/cc6cf0a96a627e678ffc996a8f9d1416200d6c81.tar.gz;
+  sha256 = "1srjikizp8ip4h42x7kr4qf00lxcp1l8zp6h0r1ddfdyw8gv9001";
+}) {} }: with pkgs;
 
-  mkShell {
-    LANG= "en_US.UTF-8";
-    FONTCONFIG_FILE = makeFontsConf { fontDirectories = [ libertine ]; };
-    buildInputs = [
-      (texlive.combine {
-        inherit (texlive)
-          scheme-medium
-          newenviron
-          catoptions
-          xstring
-          lastpage
-          libertine
-          mweights
-          fontaxes
-          pbox
-          needspace
-          fontawesome
-          realboxes
-          forloop
-          collectbox
-          cv4tw;
-      })
-      glibcLocales
-    ];
-  }
+  let
+    secrets = import ./secrets.nix;
+    buildIn = {
+      buildInputs = [
+        (texlive.combine {
+          inherit (texlive)
+            scheme-medium
+            newenviron
+            catoptions
+            xstring
+            lastpage
+            libertine
+            mweights
+            fontaxes
+            pbox
+            needspace
+            fontawesome
+            realboxes
+            forloop
+            collectbox
+            cv4tw;
+        })
+        glibcLocales
+        sops
+        gnupg
+      ];
+    };
+    params = secrets // buildIn;
+  in
+    mkShell params
