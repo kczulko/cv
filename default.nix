ea735776b8450e15e9f89d0bc63ef9b88fe87a76
Karol Czulkowski <karol.czulkowski@gmail.com>
2020-05-11 08:50:01 +0200

    Add default secrets.

--- default.nix
+++ default.nix
@@ -1,4 +1,6 @@
-{ pkgs ? import (builtins.fetchTarball {
+{
+  secrets ? import ./secrets.nix,
+  pkgs ? import (builtins.fetchTarball {
   name = "nixos-unstable-2019-12-05";
   url =  https://github.com/nixos/nixpkgs/archive/cc6cf0a96a627e678ffc996a8f9d1416200d6c81.tar.gz;
   sha256 = "1srjikizp8ip4h42x7kr4qf00lxcp1l8zp6h0r1ddfdyw8gv9001";
@@ -6,8 +8,6 @@
 
   let
     name = "kczulko-cv";
-
-    secrets = import ./secrets.nix;
 
     derivationParams = {
       name = name;
@@ -38,6 +38,7 @@
 
       buildPhase = ''
         export TEXMFVAR=$(pwd)
+        lualatex -interaction=nonstopmode $src/$name.tex
         lualatex -interaction=nonstopmode $src/$name.tex
       '';
 
---
+++ secrets-default.nix
@@ -0,0 +1,7 @@
+{
+  DATE_OF_BIRTH    = "January 1st 1970";
+  CELLPHONE        = "+48 000 000 000";
+  ADDRESS_STREET   = "Wiejska 4/6/8";
+  ADDRESS_ZIP_CITY = "00-000 Gdańsk";
+  ADDRESS_COUNTRY  = "Poland";
+}
